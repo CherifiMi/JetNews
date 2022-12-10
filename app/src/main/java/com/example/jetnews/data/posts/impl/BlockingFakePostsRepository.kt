44b74da56e2798392184102241a493f1aac88f7d
CherifiMi <cherifi154949@gmail.com>
2022-12-10 18:36:37 +0100

    data

---
+++ app/src/main/java/com/example/jetnews/data/posts/impl/BlockingFakePostsRepository.kt
@@ -0,0 +1,39 @@
+package com.example.jetnews.data.posts.impl
+
+import com.example.jetnews.data.Result
+import com.example.jetnews.data.posts.PostsRepository
+import com.example.jetnews.model.Post
+import com.example.jetnews.model.PostsFeed
+import com.example.jetnews.util.addOrRemove
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.withContext
+
+class BlockingFakePostsRepository : PostsRepository {
+
+    // for now, keep the favorites in memory
+    private val favorites = MutableStateFlow<Set<String>>(setOf())
+
+    override suspend fun getPost(postId: String?): Result<Post> {
+        return withContext(Dispatchers.IO) {
+            val post = posts.allPosts.find { it.id == postId }
+            if (post == null) {
+                Result.Error(IllegalArgumentException("Unable to find post"))
+            } else {
+                Result.Success(post)
+            }
+        }
+    }
+
+    override suspend fun getPostsFeed(): Result<PostsFeed> {
+        return Result.Success(posts)
+    }
+
+    override fun observeFavorites(): Flow<Set<String>> = favorites
+
+    override suspend fun toggleFavorite(postId: String) {
+        favorites.update { it.addOrRemove(postId) }
+    }
+}
