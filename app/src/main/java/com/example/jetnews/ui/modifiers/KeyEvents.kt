54bfd008a5ae9c4a970e06b7f913b5af1556b511
CherifiMi <cherifi154949@gmail.com>
2022-12-10 18:43:01 +0100

    Modifiers

---
+++ app/src/main/java/com/example/jetnews/ui/modifiers/KeyEvents.kt
@@ -0,0 +1,14 @@
+package com.example.jetnews.ui.modifiers
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.*
+import androidx.compose.ui.input.key.KeyEventType.Companion.KeyUp
+
+fun Modifier.interceptKey(key: Key, onKeyEvent: ()->Unit): Modifier{
+    return this.onPreviewKeyEvent{
+        if (it.key == key && it.type == KeyUp) {
+            onKeyEvent()
+            true
+        } else it.key == key
+    }
+}
