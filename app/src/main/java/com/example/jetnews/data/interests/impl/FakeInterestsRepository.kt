d93177275c13556eed1bd53d677752c72d0eb910
CherifiMi <cherifi154949@gmail.com>
2022-12-10 18:35:31 +0100

    FakeInterestsRepository

--- app/src/main/java/com/example/jetnews/data/interests/impl/FakeInterestsRepository.kt
+++ app/src/main/java/com/example/jetnews/data/interests/impl/FakeInterestsRepository.kt
@@ -4,42 +4,91 @@ import com.example.jetnews.data.Result
 import com.example.jetnews.data.interests.InterestSection
 import com.example.jetnews.data.interests.InterestsRepository
 import com.example.jetnews.data.interests.TopicSelection
+import com.example.jetnews.data.posts.impl.publication
+import com.example.jetnews.util.addOrRemove
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 
 class FakeInterestsRepository: InterestsRepository {
+
+
+    private val topics by lazy {
+        listOf(
+            InterestSection("Android", listOf("Jetpack Compose", "Kotlin", "Jetpack")),
+            InterestSection(
+                "Programming",
+                listOf("Kotlin", "Declarative UIs", "Java", "Unidirectional Data Flow", "C++")
+            ),
+            InterestSection("Technology", listOf("Pixel", "Google"))
+        )
+    }
+
+    private val people by lazy {
+        listOf(
+            "Kobalt Toral",
+            "K'Kola Uvarek",
+            "Kris Vriloc",
+            "Grala Valdyr",
+            "Kruel Valaxar",
+            "L'Elij Venonn",
+            "Kraag Solazarn",
+            "Tava Targesh",
+            "Kemarrin Muuda"
+        )
+    }
+
+    private val publications by lazy {
+        listOf(
+            "Kotlin Vibe",
+            "Compose Mix",
+            "Compose Breakdown",
+            "Android Pursue",
+            "Kotlin Watchman",
+            "Jetpack Ark",
+            "Composeshack",
+            "Jetpack Point",
+            "Compose Tribune"
+        )
+    }
+
+    private val selectedTopics = MutableStateFlow(setOf<TopicSelection>())
+    private val selectedPeople = MutableStateFlow(setOf<String>())
+    private val selectedPublications = MutableStateFlow(setOf<String>())
+
     override suspend fun getTopics(): Result<List<InterestSection>> {
-        TODO("Not yet implemented")
+        return Result.Success(topics)
     }
 
     override suspend fun getPeople(): Result<List<String>> {
-        TODO("Not yet implemented")
+        return Result.Success(people)
     }
 
     override suspend fun getPublications(): Result<List<String>> {
-        TODO("Not yet implemented")
+        return Result.Success(publications)
     }
 
     override suspend fun toggleTopicSelection(topic: TopicSelection) {
-        TODO("Not yet implemented")
+        selectedTopics.update {
+            it.addOrRemove(topic)
+        }
     }
 
     override suspend fun togglePersonSelected(person: String) {
-        TODO("Not yet implemented")
+        selectedPeople.update {
+            it.addOrRemove(person)
+        }
     }
 
     override suspend fun togglePublicationSelected(publication: String) {
-        TODO("Not yet implemented")
+        selectedPublications.update {
+            it.addOrRemove(publication)
+        }
     }
 
-    override fun observeTopicsSelected(): Flow<Set<TopicSelection>> {
-        TODO("Not yet implemented")
-    }
+    override fun observeTopicsSelected(): Flow<Set<TopicSelection>> = selectedTopics
 
-    override fun observePeopleSelected(): Flow<Set<String>> {
-        TODO("Not yet implemented")
-    }
+    override fun observePeopleSelected(): Flow<Set<String>> = selectedPeople
 
-    override fun observePublicationSelected(): Flow<Set<String>> {
-        TODO("Not yet implemented")
-    }
+    override fun observePublicationSelected(): Flow<Set<String>> = selectedPublications
 }
--- app/src/main/java/com/example/jetnews/data/posts/impl/FakePostsRepository.kt
+++ app/src/main/java/com/example/jetnews/data/posts/impl/FakePostsRepository.kt
@@ -4,22 +4,49 @@ import com.example.jetnews.data.Result
 import com.example.jetnews.data.posts.PostsRepository
 import com.example.jetnews.model.Post
 import com.example.jetnews.model.PostsFeed
+import com.example.jetnews.util.addOrRemove
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.withContext
 
 class FakePostsRepository: PostsRepository {
+
+    private val favorites = MutableStateFlow<Set<String>>(setOf())
+
     override suspend fun getPost(postId: String?): Result<Post> {
-        TODO("Not yet implemented")
+        return withContext(Dispatchers.IO){
+            val post = posts.allPosts.find { it.id == postId }
+            if (post == null){
+                Result.Error(IllegalArgumentException("Post not found"))
+            }else{
+                Result.Success(post)
+            }
+        }
     }
 
     override suspend fun getPostsFeed(): Result<PostsFeed> {
-        TODO("Not yet implemented")
+        return withContext(Dispatchers.IO) {
+            delay(800) // pretend we're on a slow network
+            if (shouldRandomlyFail()) {
+                Result.Error(IllegalStateException())
+            } else {
+                Result.Success(posts)
+            }
+        }
     }
 
-    override fun observeFavorites(): Flow<Set<String>> {
-        TODO("Not yet implemented")
-    }
+    override fun observeFavorites(): Flow<Set<String>> = favorites
 
     override suspend fun toggleFavorite(postId: String) {
-        TODO("Not yet implemented")
+        favorites.update {
+            it.addOrRemove(postId)
+        }
     }
+
+    private var requestCount = 0
+
+    private fun shouldRandomlyFail(): Boolean = ++requestCount % 5 == 0
 }
