789c39579a9c1f0dd43aa2f1cc46615d3b8aade6
CherifiMi <cherifi154949@gmail.com>
2022-12-18 03:01:53 +0100

    jetnews

--- app/src/main/java/com/example/jetnews/ui/JetnewsApp.kt
+++ app/src/main/java/com/example/jetnews/ui/JetnewsApp.kt
@@ -1,9 +1,11 @@
 package com.example.jetnews.ui
 
+import android.util.Log
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.runtime.Composable
 import com.example.jetnews.data.AppContainer
 
-@androidx.compose.runtime.Composable
+@Composable
 fun JetnewsApp(
     appContainer: AppContainer,
     widthSizeClass: WindowWidthSizeClass
