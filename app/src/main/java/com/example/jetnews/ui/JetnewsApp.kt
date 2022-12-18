a248c77bbc42c05d1a4c8517bdd365fe3e91e07d
CherifiMi <cherifi154949@gmail.com>
2022-12-18 02:55:03 +0100

    jetnews

---
+++ app/src/main/java/com/example/jetnews/ui/JetnewsApp.kt
@@ -0,0 +1,12 @@
+package com.example.jetnews.ui
+
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import com.example.jetnews.data.AppContainer
+
+@androidx.compose.runtime.Composable
+fun JetnewsApp(
+    appContainer: AppContainer,
+    widthSizeClass: WindowWidthSizeClass
+) {
+
+}
--- app/src/main/java/com/example/jetnews/ui/MainActivity.kt
+++ app/src/main/java/com/example/jetnews/ui/MainActivity.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.core.view.WindowCompat
+import com.example.jetnews.JetnewsApplication
 import com.example.jetnews.ui.theme.JetnewsTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        val appContainer = (application as JetnewsApplication).container
         setContent {
-            JetnewsTheme {
-                Text(text = "hi mito")
-            }
+            val widthSizeClass = calculateWindowSizeClass(this).widthSizeClass
+            JetnewsApp(appContainer, widthSizeClass)
         }
     }
 }
