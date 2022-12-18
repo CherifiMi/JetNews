07552fc60aa377bb0726a85759d9315c755ebfb6
CherifiMi <cherifi154949@gmail.com>
2022-12-18 03:06:04 +0100

    jetnews

--- app/src/main/java/com/example/jetnews/ui/components/AppNavRail.kt
+++ app/src/main/java/com/example/jetnews/ui/components/AppNavRail.kt
@@ -1,8 +1,30 @@
 package com.example.jetnews.ui.components
 
+import android.content.res.Configuration
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetnews.ui.theme.JetnewsTheme
+import com.example.jetnews.ui.util.JetnewsDestinations
 
 @Composable
-fun AppNavRail() {
+fun AppNavRail(
+    currentRoute: String,
+    navigateToHome: () -> Unit,
+    navigateToInterests: () -> Unit,
+    modifier: Modifier = Modifier
+) {
 
+}
+@Preview("Drawer contents")
+@Preview("Drawer contents (dark)", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun PreviewAppNavRail() {
+    JetnewsTheme {
+        AppNavRail(
+            currentRoute = JetnewsDestinations.HOME_ROUTE,
+            navigateToHome = {},
+            navigateToInterests = {},
+        )
+    }
 }
---
+++ app/src/main/java/com/example/jetnews/ui/util/JetnewsDestinations.kt
@@ -0,0 +1,6 @@
+package com.example.jetnews.ui.util
+
+object JetnewsDestinations {
+    const val HOME_ROUTE = "home"
+    const val INTERESTS_ROUTE = "interests"
+}
