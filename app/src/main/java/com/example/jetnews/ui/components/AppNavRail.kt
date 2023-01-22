7e1c7ca1c631d3b25b4996adcbc4a27ce064068e
CherifiMi <cherifi154949@gmail.com>
2023-01-22 05:24:33 +0100

    all

--- app/src/main/java/com/example/jetnews/ui/components/AppNavRail.kt
+++ app/src/main/java/com/example/jetnews/ui/components/AppNavRail.kt
@@ -1,9 +1,20 @@
 package com.example.jetnews.ui.components
 
 import android.content.res.Configuration
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.ListAlt
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetnews.R
 import com.example.jetnews.ui.theme.JetnewsTheme
 import com.example.jetnews.ui.util.JetnewsDestinations
 
@@ -14,8 +25,36 @@ fun AppNavRail(
     navigateToInterests: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-
+    NavigationRail(
+        header = {
+            Icon(
+                painterResource(R.drawable.ic_jetnews_logo),
+                null,
+                Modifier.padding(vertical = 12.dp),
+                tint = MaterialTheme.colorScheme.primary
+            )
+        },
+        modifier = modifier
+    ) {
+        Spacer(Modifier.weight(1f))
+        NavigationRailItem(
+            selected = currentRoute == JetnewsDestinations.HOME_ROUTE,
+            onClick = navigateToHome,
+            icon = { Icon(Icons.Filled.Home, stringResource(R.string.home_title)) },
+            label = { Text(stringResource(R.string.home_title)) },
+            alwaysShowLabel = false
+        )
+        NavigationRailItem(
+            selected = currentRoute == JetnewsDestinations.INTERESTS_ROUTE,
+            onClick = navigateToInterests,
+            icon = { Icon(Icons.Filled.ListAlt, stringResource(R.string.interests_title)) },
+            label = { Text(stringResource(R.string.interests_title)) },
+            alwaysShowLabel = false
+        )
+        Spacer(Modifier.weight(1f))
+    }
 }
+
 @Preview("Drawer contents")
 @Preview("Drawer contents (dark)", uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
